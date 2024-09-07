10062e20d4f4ad096b1c9784c7accd2b26cf9b39
christwosix <christwosix@duck.com>
2024-09-07 14:30:20 +1000

    all: initial commit

---
+++ shapes/struct.go
@@ -0,0 +1,17 @@
+package shapes
+
+// Square represents attributes for squares.
+type Square struct {
+	Side float64
+}
+
+// Rectangle represents attributes for rectangles.
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+// Circle represents attributes for circles.
+type Circle struct {
+	Radius float64
+}
